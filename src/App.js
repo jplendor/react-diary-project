db051101d4b631a40615bd1b44ff420968684c4a
jplendor <jplendor@gmail.com>
2022-04-17 19:40:31 +0900

    API 호출 (init data 가져오기)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,28 @@ function App() {
 
   const dataId = useRef(0);
 
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((item) => {
+      return {
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getDate(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
